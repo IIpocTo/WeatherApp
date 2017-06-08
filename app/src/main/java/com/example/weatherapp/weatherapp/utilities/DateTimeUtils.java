4b4a7855abc6d79ee03b449a9fecb16cb40cfb44
vlad1000 <allxf95@gmail.com>
2017-06-08 18:13:24 +0300

    Fix dateTime representation

--- app/src/main/java/com/example/weatherapp/weatherapp/utilities/DateTimeUtils.java
+++ app/src/main/java/com/example/weatherapp/weatherapp/utilities/DateTimeUtils.java
@@ -7,7 +7,6 @@ import com.example.weatherapp.weatherapp.R;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.TimeZone;
 
 public final class DateTimeUtils {
 
@@ -16,13 +15,12 @@ public final class DateTimeUtils {
     private static final int HOURS_IN_MILLIS = MINUTE_IN_MILLIS * 60;
     public static final int DAY_IN_MILLIS = HOURS_IN_MILLIS * 24;
 
-    private static final String TODAY_AND_TOMORROW_DATE_FORMAT = "k:mm";
-    private static final String REST_DAYS_DATE_FORMAT = "EEEE MMMM, dd k:mm";
+    private static final String TODAY_AND_TOMORROW_DATE_FORMAT = "HH:mm";
+    private static final String REST_DAYS_DATE_FORMAT = "EEEE MMMM, dd HH:mm";
 
     private static String convertMillisToFormattedDate(long dateInMillis, String dateFormat) {
         Date date = new Date(dateInMillis * 1000L);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.getDefault());
-        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT-4"));
         return simpleDateFormat.format(date);
     }
 
@@ -38,7 +36,6 @@ public final class DateTimeUtils {
     public static String getReadableDateString(Context context, long dateInMillis) {
 
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
-        formatter.setTimeZone(TimeZone.getTimeZone("GMT-4"));
 
         if (isToday(formatter, dateInMillis)) {
             String formattedDate = convertMillisToFormattedDate(dateInMillis, TODAY_AND_TOMORROW_DATE_FORMAT);
