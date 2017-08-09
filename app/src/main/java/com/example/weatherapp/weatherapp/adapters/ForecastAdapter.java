54d79f7c6ec41bb6a72ab0681a9328f886ae9be0
vlad1000 <allxf95@gmail.com>
2017-08-09 04:39:09 +0300

    Update main page view

--- app/src/main/java/com/example/weatherapp/weatherapp/adapters/ForecastAdapter.java
+++ app/src/main/java/com/example/weatherapp/weatherapp/adapters/ForecastAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.weatherapp.weatherapp.R;
@@ -44,15 +45,32 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
     @Override
     public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+
         mCursor.moveToPosition(position);
+
         long date = mCursor.getLong(WeatherEntry.INDEX_COLUMN_DATE);
         String dateString = DateTimeUtils.getReadableDateString(mContext, date);
+        holder.dateView.setText(dateString);
+
         int weatherConditionId = mCursor.getInt(WeatherEntry.INDEX_COLUMN_WEATHER_ID);
+        int iconId = WeatherUtils.getSmallArtResourceIdForWeatherCondition(weatherConditionId);
+        holder.iconView.setImageResource(iconId);
+
         String description = WeatherUtils.getStringForWeatherCondition(mContext, weatherConditionId);
-        double min = mCursor.getDouble(WeatherEntry.INDEX_COLUMN_MIN);
-        double max = mCursor.getDouble(WeatherEntry.INDEX_COLUMN_MAX);
-        String highLowTemp = WeatherUtils.formatHighLows(mContext, min, max);
-        holder.mWeatherTextView.setText(dateString + " - " + description + " - " + highLowTemp);
+        String descriptionA11y = mContext.getString(R.string.a11y_forecast, description);
+        holder.descriptionView.setText(description);
+        holder.descriptionView.setContentDescription(descriptionA11y);
+
+        double lowTemp = mCursor.getDouble(WeatherEntry.INDEX_COLUMN_MIN);
+        String lowTempA11y = mContext.getString(R.string.a11y_low_temp, String.valueOf(lowTemp));
+        holder.lowTempView.setText(WeatherUtils.formatTemperature(mContext, lowTemp));
+        holder.lowTempView.setContentDescription(lowTempA11y);
+
+        double highTemp = mCursor.getDouble(WeatherEntry.INDEX_COLUMN_MAX);
+        String highTempA11y = mContext.getString(R.string.a11y_high_temp, String.valueOf(highTemp));
+        holder.highTempView.setText(WeatherUtils.formatTemperature(mContext, highTemp));
+        holder.highTempView.setContentDescription(highTempA11y);
+
     }
 
     @Override
@@ -62,11 +80,19 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
     public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private final TextView mWeatherTextView;
+        final ImageView iconView;
+        final TextView dateView;
+        final TextView descriptionView;
+        final TextView highTempView;
+        final TextView lowTempView;
 
         public ForecastAdapterViewHolder(View itemView) {
             super(itemView);
-            mWeatherTextView = (TextView) itemView.findViewById(R.id.tv_weather_data);
+            iconView = (ImageView) itemView.findViewById(R.id.weather_icon);
+            dateView = (TextView) itemView.findViewById(R.id.date);
+            descriptionView = (TextView) itemView.findViewById(R.id.weatherDescription);
+            highTempView = (TextView) itemView.findViewById(R.id.highTemp);
+            lowTempView = (TextView) itemView.findViewById(R.id.lowTemp);
             itemView.setOnClickListener(this);
         }
 
